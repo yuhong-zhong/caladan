6e862973bfd41bf6db219a06c320aa6ac31f7994
Josh Fried <joshuafried@gmail.com>
2023-04-04 02:26:06 +0000

    syscall nits

--- inc/base/syscall.h
+++ inc/base/syscall.h
@@ -6,6 +6,7 @@
 
 #include <stddef.h>
 #include <sys/types.h>
+#include <sys/uio.h>
 
 #include <base/compiler.h>
 
@@ -22,4 +23,28 @@ extern int syscall_ioctl(int fd, unsigned long int request, void *arg);
 extern int syscall_madvise(void *addr, size_t length, int advice);
 extern int syscall_mprotect(void *addr, size_t len, int prot);
 extern void syscall_exit(int status) __noreturn;
-extern ssize_t syscall_write(int fd, const void *buf, size_t count);
+extern ssize_t syscall_pwritev2(int fd, const struct iovec *iov, int iovcnt,
+	                            off_t offset_lo, off_t offset_hi, int flags);
+
+extern ssize_t syscall_writev(int fd, const struct iovec *iov, int iovcnt);
+
+static inline ssize_t syscall_pwrite(int fd, const void *buf, size_t count,
+	                                 off_t offset)
+{
+	const struct iovec iov = {
+		.iov_base = (void *)buf,
+		.iov_len = count
+	};
+
+	return syscall_pwritev2(fd, &iov, 1, offset, 0, 0);
+}
+
+static inline ssize_t syscall_write(int fd, const void *buf, size_t count)
+{
+	const struct iovec iov = {
+		.iov_base = (void *)buf,
+		.iov_len = count
+	};
+
+	return syscall_writev(fd, &iov, 1);
+}

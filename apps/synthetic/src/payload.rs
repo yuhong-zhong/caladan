c7b8f4d2d6a39f465b438b62c6a800a443ebe11f
Yuhong Zhong <yz@cs.columbia.edu>
2024-11-18 22:25:35 +0000

    synthetic: enable variable payload size

--- apps/synthetic/src/payload.rs
+++ apps/synthetic/src/payload.rs
@@ -1,8 +1,11 @@
+extern crate clap;
+
 use crate::Buffer;
 use crate::Connection;
 use crate::LoadgenProtocol;
 use crate::Packet;
 use crate::Transport;
+use clap::{App, Arg};
 
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use std::io;
@@ -12,12 +15,17 @@ pub struct Payload {
     pub work_iterations: u64,
     pub index: u64,
     pub randomness: u64,
+    pub extra_payload_size: u64,
 }
 
-pub const PAYLOAD_SIZE: usize = 24;
+pub const PAYLOAD_SIZE: usize = 32;
+pub const MAX_EXTRA_PAYLOAD_SIZE: usize = 8192;
+pub const MAX_TOTAL_SIZE: usize = PAYLOAD_SIZE + MAX_EXTRA_PAYLOAD_SIZE;
 
 #[derive(Clone, Copy)]
-pub struct SyntheticProtocol {}
+pub struct SyntheticProtocol {
+    pub extra_payload_size: usize,
+}
 
 impl LoadgenProtocol for SyntheticProtocol {
     fn uses_ordered_requests(&self) -> bool {
@@ -29,6 +37,7 @@ impl LoadgenProtocol for SyntheticProtocol {
             work_iterations: p.work_iterations,
             index: i as u64,
             randomness: p.randomness,
+            extra_payload_size: self.extra_payload_size as u64,
         }
         .serialize_into(buf)
         .unwrap();
@@ -44,11 +53,21 @@ impl LoadgenProtocol for SyntheticProtocol {
 
 impl SyntheticProtocol {
     pub fn with_args(_matches: &clap::ArgMatches, _tport: Transport) -> Self {
-        SyntheticProtocol {}
+        let extra_payload_size = value_t_or_exit!(_matches, "extra_payload_size", usize);
+        assert!(extra_payload_size <= MAX_EXTRA_PAYLOAD_SIZE);
+        SyntheticProtocol {
+            extra_payload_size: extra_payload_size,
+        }
     }
 
     pub fn args<'a, 'b>() -> Vec<clap::Arg<'a, 'b>> {
-        vec![]
+        vec![
+            Arg::with_name("extra_payload_size")
+                .long("extra_payload_size")
+                .takes_value(true)
+                .default_value("0")
+                .help("Synthetic: size of extra payload"),
+        ]
     }
 }
 
@@ -57,6 +76,8 @@ impl Payload {
         writer.write_u64::<BigEndian>(self.work_iterations)?;
         writer.write_u64::<BigEndian>(self.index)?;
         writer.write_u64::<BigEndian>(self.randomness)?;
+        writer.write_u64::<BigEndian>(self.extra_payload_size)?;
+        writer.write_all(&vec![0; self.extra_payload_size as usize])?;
         Ok(())
     }
 
@@ -65,6 +86,7 @@ impl Payload {
             work_iterations: reader.read_u64::<BigEndian>()?,
             index: reader.read_u64::<BigEndian>()?,
             randomness: reader.read_u64::<BigEndian>()?,
+            extra_payload_size: reader.read_u64::<BigEndian>()?,
         };
         return Ok(p);
     }

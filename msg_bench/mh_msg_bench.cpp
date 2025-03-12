a49c675485e73ead936d0d090ce676e29b4e050f
Yuhong Zhong <yz@cs.columbia.edu>
2025-03-12 03:10:42 +0000

    mh_msg_bench: remove 1MB read

--- msg_bench/mh_msg_bench.cpp
+++ msg_bench/mh_msg_bench.cpp
@@ -383,8 +383,8 @@ void run_on_core(uint64_t core) {
 
 #define LAT_SAMPLE_RATE (1000ul)
 
-#define NR_BLOCKS (16384ul)
-#define BLOCK_SIZE (1ul << 20ul)
+// #define NR_BLOCKS (16384ul)
+// #define BLOCK_SIZE (1ul << 20ul)
 
 void consumer_thread_fn(uint8_t *cxl_buf, uint64_t num_iterations) {
 	uint8_t *cxl_buf_start = cxl_buf;
@@ -415,9 +415,9 @@ void consumer_thread_fn(uint8_t *cxl_buf, uint64_t num_iterations) {
 	// memset(latency_buf, 0, num_samples * sizeof(uint64_t));
 	// uint64_t lat_index = 0;
 
-	uint8_t *local_buf = (uint8_t *) aligned_alloc(PAGE_SIZE, BLOCK_SIZE);
-	BUG_ON(local_buf == NULL);
-	memset(local_buf, 0, BLOCK_SIZE);
+	// uint8_t *local_buf = (uint8_t *) aligned_alloc(PAGE_SIZE, BLOCK_SIZE);
+	// BUG_ON(local_buf == NULL);
+	// memset(local_buf, 0, BLOCK_SIZE);
 
 	uint64_t cmd;
 	unsigned long payload;
@@ -434,13 +434,13 @@ void consumer_thread_fn(uint8_t *cxl_buf, uint64_t num_iterations) {
 		}
 		BUG_ON(cmd != i);
 
-		uint64_t block_index = i % NR_BLOCKS;
-		batch_clflushopt(cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
-		memcpy(local_buf, cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
+		// uint64_t block_index = i % NR_BLOCKS;
+		// batch_clflushopt(cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
+		// memcpy(local_buf, cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
 
-		block_index = (i % NR_BLOCKS) + NR_BLOCKS;
-		memcpy(cxl_buf + block_index * BLOCK_SIZE, local_buf, BLOCK_SIZE);
-		batch_clflushopt(cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
+		// block_index = (i % NR_BLOCKS) + NR_BLOCKS;
+		// memcpy(cxl_buf + block_index * BLOCK_SIZE, local_buf, BLOCK_SIZE);
+		// batch_clflushopt(cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
 
 		if (i % LAT_SAMPLE_RATE == LAT_SAMPLE_RATE - 1) {
 			// uint64_t now = __rdtsc();
@@ -475,9 +475,9 @@ void sender_reverse_thread_fn(struct msg_chan_in *reverse_chan, uint64_t num_ite
 	BUG_ON(latency_buf == NULL);
 	memset(latency_buf, 0, num_samples * sizeof(uint64_t));
 
-	uint8_t *local_buf = (uint8_t *) aligned_alloc(PAGE_SIZE, BLOCK_SIZE);
-	BUG_ON(local_buf == NULL);
-	memset(local_buf, 0, BLOCK_SIZE);
+	// uint8_t *local_buf = (uint8_t *) aligned_alloc(PAGE_SIZE, BLOCK_SIZE);
+	// BUG_ON(local_buf == NULL);
+	// memset(local_buf, 0, BLOCK_SIZE);
 
 	for (uint64_t i = 0; i < num_samples; i++) {
 		// while (!msg_recv(reverse_chan, &cmd, &payload)) {
@@ -487,9 +487,9 @@ void sender_reverse_thread_fn(struct msg_chan_in *reverse_chan, uint64_t num_ite
 			pause();
 		}
 
-		uint64_t block_index = (i % NR_BLOCKS) + NR_BLOCKS;
-		batch_clflushopt(cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
-		memcpy(local_buf, cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
+		// uint64_t block_index = (i % NR_BLOCKS) + NR_BLOCKS;
+		// batch_clflushopt(cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
+		// memcpy(local_buf, cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
 
 		uint64_t now = __rdtsc();
 		latency_buf[i] = now - payload;
@@ -539,9 +539,9 @@ void sender_thread_fn(uint8_t *cxl_buf, uint64_t num_iterations, uint64_t delay_
 	reverse_chan.prefetch_len = 16;
 	cxl_buf += ROUND_UP(CHAN_SIZE * sizeof(struct lrpc_msg), CACHE_LINE_SIZE);
 
-	uint8_t *local_buf = (uint8_t *) aligned_alloc(PAGE_SIZE, BLOCK_SIZE);
-	BUG_ON(local_buf == NULL);
-	memset(local_buf, 0, BLOCK_SIZE);
+	// uint8_t *local_buf = (uint8_t *) aligned_alloc(PAGE_SIZE, BLOCK_SIZE);
+	// BUG_ON(local_buf == NULL);
+	// memset(local_buf, 0, BLOCK_SIZE);
 
 	thread reverse_thread(sender_reverse_thread_fn, &reverse_chan, num_iterations, cxl_buf);
 	sleep(1);
@@ -561,9 +561,9 @@ void sender_thread_fn(uint8_t *cxl_buf, uint64_t num_iterations, uint64_t delay_
 		// 	pause();
 		// }
 
-		uint64_t block_index = i % NR_BLOCKS;
-		memcpy(cxl_buf + block_index * BLOCK_SIZE, local_buf, BLOCK_SIZE);
-		batch_clflushopt(cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
+		// uint64_t block_index = i % NR_BLOCKS;
+		// memcpy(cxl_buf + block_index * BLOCK_SIZE, local_buf, BLOCK_SIZE);
+		// batch_clflushopt(cxl_buf + block_index * BLOCK_SIZE, BLOCK_SIZE);
 
 		while (!huge_msg_send(&chan_out, i, now)) {
 			pause();
